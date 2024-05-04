b23330f2054dda17c2366ab1cc8eb245bfb2c5cd
tahir-mansour <tahirelmag@gmail.com>
2024-05-04 08:34:55 +0300

    changed colours in production

--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
     >
        <html lang="en">
           <body className={cn(
-          "min-h-screen bg-background font-sans antialiased ",
+          "min-h-screen bg-white dark:bg-black dark:text-white font-sans antialiased ",
           fontSans.variable
            )}>
             
